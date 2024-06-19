fc24f6b6aac4c7454747f1bd674f7125a7713202
EHAB3x <ehabmah6969@gmail.com>
2024-06-19 21:08:21 +0200

    change routing to best practice way

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,8 @@
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// Layouts
-import MainLayout from "@layouts/MainLayout/MainLayout";
-// Pages
-import Home from '@pages/Home';
-import Categories from '@pages/Categories';
-import Products from '@pages/Products';
-import AboutUs from '@pages/AboutUs';
+
 // Styles
 import "bootstrap/dist/css/bootstrap.min.css";
+import AppRouter from '@routes/AppRouter';
 
-const router = createBrowserRouter([{
-  path:"/",
-  element:<MainLayout />,
-  children:[
-    {
-    index:true,
-    element:<Home />
-    },
-    {
-      path:"categories",
-      element:<Categories />
-    },
-    {
-      path:"products/:prefix",
-      element:<Products />
-    },
-    {
-      path:"about-us",
-      element:<AboutUs />
-    },
-  ]
-}])
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router}/>
-)
+
+ReactDOM.createRoot(document.getElementById('root')!).render( <AppRouter />);
