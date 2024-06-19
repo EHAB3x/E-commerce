ea8b3e5a963819041379a0cb904fa58ff9f78f90
EHAB3x <ehabmah6969@gmail.com>
2024-06-19 19:20:31 +0200

    add bootstrap navbar

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Container } from "react-bootstrap";
 import styles from "./styles.module.css";
+import Header from "../../components/common/Header/Header";
 
 const {container, wrapper} = styles;
 
 const MainLayout = () => {
   return (
     <Container className={container}>
+        <Header />
         <div className={wrapper}></div>
     </Container>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client'
+import "bootstrap/dist/css/bootstrap.min.css"
 import MainLayout from './layouts/MainLayout/MainLayout'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<MainLayout />)
