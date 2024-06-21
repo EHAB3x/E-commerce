bac7e1580f1526a5774dec926e5a12911d33af01
EHAB3x <ehabmah6969@gmail.com>
2024-06-21 08:15:32 +0200

    finish project nav links

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client'
+import AppRouter from '@routes/AppRouter';
 
 // Styles
 import "bootstrap/dist/css/bootstrap.min.css";
-import AppRouter from '@routes/AppRouter';
-
+import "@styles/global.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render( <AppRouter />);
