d356a30edd9befe36eb3bf8bd7902e23a47ed27e
EHAB3x <ehabmah6969@gmail.com>
2024-06-21 11:04:40 +0200

    fetch categories and render it

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -12,34 +12,23 @@ const Categories = () => {
   useEffect(() => {
     dispatch(actGetCategories());
   }, [dispatch]);
+
+  const categoriesList =
+    records.length > 0
+      ? records.map((record) => (
+          <Col
+            xs={6}
+            md={3}
+            key={record.id}
+            className="d-flex justify-content-center mb-5 mt-2"
+          >
+            <Category {...record} />
+          </Col>
+        ))
+      : "There are no categories";
   return (
     <Container>
-      <Row>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-      </Row>
+      <Row>{categoriesList}</Row>
     </Container>
   );
 };
