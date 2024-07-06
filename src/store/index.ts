64df9b4613e815c859519586dbf9b0cda4b0349e
EHAB3x <ehabmah6969@gmail.com>
2024-07-06 13:30:19 +0200

    store cart items in redux persist instead of local storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import categories from "./categories/categoriesSlice";
 import products from "./products/productsSlice";
 import cart from "./cart/cartSlice";
 
-export const store = configureStore({
-  reducer: { categories, products, cart },
+const rootPersistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
+  categories,
+  products,
+  cart,
+});
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+const persistor = persistStore(store);
+export { store, persistor };
