8ad6783a44b5aac38a1b3d754a7f2850a1d122d0
EHAB3x <ehabmah6969@gmail.com>
2024-07-06 10:21:02 +0200

    show total number of items in cart icon

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -1,14 +1,24 @@
+import { useAppSelector } from "@store/hooks";
 import Logo from "@assets/svg/cart.svg?react";
 import styles from "./styles.module.css";
 
-const {basketContainer, basketQuantity} = styles;
+const { basketContainer, basketQuantity } = styles;
+
 const HeaderBasket = () => {
+  const cartItems = useAppSelector((state) => state.cart.items);
+
+  const totalQuantity = Object.values(cartItems).reduce(
+    (accumulator, currentValue) => {
+      return accumulator + currentValue;
+    },
+    0
+  );
   return (
     <div className={basketContainer}>
-        <Logo title="basket icon"/>
-        <div className={basketQuantity}>0</div>
+      <Logo title="basket icon" />
+      <div className={basketQuantity}>{totalQuantity}</div>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderBasket
+export default HeaderBasket;
