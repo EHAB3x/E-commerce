3c7f3cd89a1f56d5094969ba7dcce636dda8f554
EHAB3x <ehabmah6969@gmail.com>
2024-07-08 11:08:02 +0200

    refactor add to cart button debounce  code

--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -9,25 +9,23 @@ const { product, productImg } = styles;
 
 const Product = ({ id, title, price, img }: TProduct) => {
   const dispatch = useAppDispatch();
-  const [isBtnClicked, setIsBtnClicked] = useState(0);
   const [isBtnDisabled, setIsBtnDisabled] = useState(false);
 
   useEffect(() => {
-    if (!isBtnClicked) {
+    if (!isBtnDisabled) {
       return;
     }
-    setIsBtnDisabled(true);
 
     const debounce = setTimeout(() => {
       setIsBtnDisabled(false);
     }, 300);
 
     return () => clearTimeout(debounce);
-  }, [isBtnClicked]);
+  }, [isBtnDisabled]);
 
   const addToCartHandler = () => {
     dispatch(addToCart(id));
-    setIsBtnClicked((prev) => prev + 1);
+    setIsBtnDisabled(true);
   };
   return (
     <div className={product}>
