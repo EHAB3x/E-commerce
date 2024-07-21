1117d3ff4577473bb6c6c7deee94d9c4cdcd7bc8
EHAB3x <ehabmah6969@gmail.com>
2024-07-21 05:54:14 +0200

    finish cart item and cart subtotal price components

--- src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
+++ src/components/eCommerce/HeaderBasket/HeaderBasket.tsx
@@ -3,9 +3,11 @@ import { useAppSelector } from "@store/hooks";
 import Logo from "@assets/svg/cart.svg?react";
 import styles from "./styles.module.css";
 import { getCartTotalQuantitySelector } from "@store/cart/cartSlice";
+import { useNavigate } from "react-router-dom";
 const { basketContainer, basketQuantity, pumpCartQuantity, basketCart } = styles;
 
 const HeaderBasket = () => {
+  const navigate = useNavigate();
   const [isAnimate, setIsAnimate] = useState(false);
   const totalQuantity = useAppSelector(getCartTotalQuantitySelector);
   const quantityStyle = `${basketQuantity} ${
@@ -25,7 +27,7 @@ const HeaderBasket = () => {
     return () => clearTimeout(debounce);
   }, [totalQuantity]);
   return (
-    <div className={basketContainer}>
+    <div className={basketContainer} onClick={()=> navigate("/cart")}>
       <div className={basketCart}>
         <Logo title="basket icon" />
         <div className={quantityStyle}>{totalQuantity}</div>
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,14 @@
 import {Heading} from "@components/common";
+import { CartItem, CartSubtotalPrice } from "@components/eCommerce";
 
 const Cart = () => {
   return (
     <>
         <Heading>Cart</Heading>
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartSubtotalPrice />
     </>
   )
 }
